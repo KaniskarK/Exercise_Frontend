b70fac13cd089a26e5d3cb8922ec7416d9c1cbce
KaniskarK <kaniskark@outlook.com>
2023-03-01 23:40:43 +0530

    Updates

---
+++ precache-manifest.83db8a10cdd8a56d7df038c0d2154324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b495a35dff06efc6ce7bb08122ef028",
+    "url": "/Exercise_Frontend/index.html"
+  },
+  {
+    "revision": "bada5ecbba715357aa77",
+    "url": "/Exercise_Frontend/static/css/2.237ca462.chunk.css"
+  },
+  {
+    "revision": "41fa3ebad8dd306a600e",
+    "url": "/Exercise_Frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bada5ecbba715357aa77",
+    "url": "/Exercise_Frontend/static/js/2.b6a3cc88.chunk.js"
+  },
+  {
+    "revision": "41fa3ebad8dd306a600e",
+    "url": "/Exercise_Frontend/static/js/main.895c8df3.chunk.js"
+  },
+  {
+    "revision": "75672c9a40d12f8193a4",
+    "url": "/Exercise_Frontend/static/js/runtime~main.c79cdd6b.js"
+  }
+]);
